e2b6a027fed0e921f1eb941e47ab51681c68e00b
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-28 17:01:10 +0200

    add sfumatura su setting page

--- src/components/MySettingPage.jsx
+++ src/components/MySettingPage.jsx
@@ -1,13 +1,19 @@
 import { Component } from "react";
+import logo from "../assets/img/netflix_logo.png";
 import { Col, Container, Row } from "react-bootstrap";
 
 class MySettingPage extends Component {
   render() {
     return (
       <main className="d-none">
+        <div className="py-3" style={{ background: "linear-gradient(180deg, rgba(1, 1, 10, 1) 0%, rgba(26, 29, 32, 1) 64%)" }}>
+          <a href="#">
+            <img src={logo} width="150px" alt="" />
+          </a>
+        </div>
         <Container className="my-5">
           <Row className="justify-content-center">
-            <Col sm="10" >
+            <Col sm="10">
               <h1>Account</h1>
               <hr />
               <Row>
@@ -22,28 +28,19 @@ class MySettingPage extends Component {
                 <Col lg="8">
                   <div className="d-flex justify-content-between">
                     <p className="fw-semibold">student@strive.school</p>
-                    <a
-                      href="#"
-                      className="text-decoration-none d-none d-sm-block"
-                    >
+                    <a href="#" className="text-decoration-none d-none d-sm-block">
                       Change account email
                     </a>
                   </div>
                   <div className="d-flex justify-content-between">
                     <p className="text-body-tertiary">Password: ***********</p>
-                    <a
-                      href="#"
-                      className="text-decoration-none d-none d-sm-block"
-                    >
+                    <a href="#" className="text-decoration-none d-none d-sm-block">
                       Change password
                     </a>
                   </div>
                   <div className="d-flex justify-content-between">
                     <p className="text-body-tertiary">Phone: 322 1456 4567</p>
-                    <a
-                      href="#"
-                      className="text-decoration-none d-none d-sm-block"
-                    >
+                    <a href="#" className="text-decoration-none d-none d-sm-block">
                       Change phone number
                     </a>
                   </div>
@@ -53,14 +50,7 @@ class MySettingPage extends Component {
                   <hr />
                   <div className="d-flex justify-content-between">
                     <div className="d-inline">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-paypal d-inline"
-                        viewBox="0 0 16 16"
-                      >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-paypal d-inline" viewBox="0 0 16 16">
                         <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.35.35 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91q.57-.403.993-1.005a4.94 4.94 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.7 2.7 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.7.7 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016q.326.186.548.438c.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.87.87 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.35.35 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32z" />
                       </svg>
                       <p className="d-inline fw-semibold">
@@ -97,14 +87,7 @@ class MySettingPage extends Component {
                   <div className="d-flex justify-content-between">
                     <div>
                       <strong>Standard </strong>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="25"
-                        fill="currentColor"
-                        className="bi bi-badge-hd"
-                        viewBox="0 0 16 16"
-                      >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-badge-hd" viewBox="0 0 16 16">
                         <path d="M7.396 11V5.001H6.209v2.44H3.687V5H2.5v6h1.187V8.43h2.522V11zM8.5 5.001V11h2.188c1.811 0 2.685-1.107 2.685-3.015 0-1.894-.86-2.984-2.684-2.984zm1.187.967h.843c1.112 0 1.622.686 1.622 2.04 0 1.353-.505 2.02-1.622 2.02h-.843z" />
                         <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
                       </svg>
@@ -157,11 +140,7 @@ class MySettingPage extends Component {
                 <Col lg="8">
                   <div className="d-flex justify-content-between">
                     <div>
-                      <img
-                        src="./assets/kids_icon.png"
-                        alt=""
-                        style={{ height: "25px" }}
-                      />
+                      <img src="./assets/kids_icon.png" alt="" style={{ height: "25px" }} />
                       <span className="fw-semibold">Strive Student</span>
                     </div>
                     <div>
